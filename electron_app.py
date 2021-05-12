db19500fe2772f4219e90bc454b154c76924bf3c
cnuebred <jonatann01ew@gmail.com>
2021-05-12 21:49:12 +0200

    updated electron app

--- electron_app.py
+++ electron_app.py
@@ -1,7 +1,14 @@
 import discord
 
+from src.core.database.mongo_controller import Connection_mongo
+from src.events.event_node import event_node
 from src.settings import TOKEN
 
 bot = discord.Client()
+
+Connection_mongo().connect()
+# setup connection for mongo db
+
+event_node(bot)
 
 bot.run(TOKEN)
