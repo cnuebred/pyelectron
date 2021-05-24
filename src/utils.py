7d0e715abbbc9553d4dbf7d1ab97e01e910a4472
cnuebred <jonatann01ew@gmail.com>
2021-05-24 21:50:01 +0200

    prettier utils

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
 async def is_member(user, guild):
     if not (isinstance(user, str) or isinstance(user, int)):
         return await guild.fetch_member(int(user.id))
-    else:
-        return await guild.fetch_member(int(user))
-        # raise TypeError("User must by specyfied by str or int (id)")
+    return await guild.fetch_member(int(user))
+    # raise TypeError("User must by specyfied by str or int (id)")
 
 
 TERMINAL_COLORS = {
