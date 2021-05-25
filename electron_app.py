42d9d9e3d52d23f067c22d1ec290bdc1c140da2d
cnuebred <jonatann01ew@gmail.com>
2021-05-25 14:47:44 +0200

    remove mongo_controller

--- electron_app.py
+++ electron_app.py
@@ -1,12 +1,10 @@
 import discord
 
-from src.core.database.mongo_controller import ConnectionMongo
 from src.events.event_node import event_node
 from src.settings import TOKEN
 
 bot = discord.Client()
 
-ConnectionMongo().connect()
 # setup connection for mongo db
 
 event_node(bot)
